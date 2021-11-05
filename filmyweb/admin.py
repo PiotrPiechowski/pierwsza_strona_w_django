0428f2b1586a882ffb95b50135c653dba9e6c2cd
Piotr Piechowski <piechowski94@gmail.com>
2021-11-05 12:31:51 +0100

    Add new files

---
+++ filmyweb/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from.models import Aktor, DodatkoweInfo, Film, Ocena
+
+# admin.site.register(Film)
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    # fields = ["tytul", "opis", "rok"]
+    # exclude = ["opis"]
+    list_display = ["tytul", "imbd_rating", "rok"]
+    list_filter = ("rok",)
+    search_fields = ("tytul", "opis",)
+
+admin.site.register(DodatkoweInfo)
+admin.site.register(Ocena)
+admin.site.register(Aktor)
---
+++ filmyweb/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+from .models import Film, DodatkoweInfo, Ocena
+
+class FilmForm(ModelForm):
+    class Meta:
+        model = Film
+        fields = ['tytul', 'rok', 'opis', 'premiera', 'imbd_rating', 'plakat']
+
+class DodatkoweInfoForm(ModelForm):
+    class Meta:
+        model = DodatkoweInfo
+        fields = ['czas_trwania', 'gatunek']
+
+
+
+class OcenaForm(ModelForm):
+    class Meta:
+        model = Ocena
+        fields = ['gwiazdki', 'recenzja']
+
+
